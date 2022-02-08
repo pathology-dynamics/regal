01174b9a1489dc44e80679dc6bfdb8e8457512e1
David_Kartchner <dkartchner3@gatech.edu>
2022-02-08 15:18:13 -0500

    remove unnecessary files

--- evaluate_rules.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-from tqdm import tqdm, trange
-
-from snorkel.labeling import LabelingFunction
-from snorkel.preprocess.nlp import SpacyPreprocessor
-from snorkel.labeling import LFAnalysis, PandasLFApplier
-from snorkel.preprocess import preprocessor
--- process_data_for_models.py
+++
@@ -1,9 +0,0 @@
-import torch
-import numpy as np
-import pandas as pd 
-
-from tqdm.auto import tqdm
-
-
-def process_data_for_rpn():
-    
